684515c738dae4760a7a00670528dd70cccb7303
David Santacruz <davidsantacruz09@gmail.com>
2021-01-04 03:26:23 -0600

    Pog

--- server/config/config.js
+++ server/config/config.js
@@ -9,9 +9,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDb;
 
 if ( process.env.NODE_ENV === 'dev' ) {
-    urlDb = 'mongodb+srv://santacruz:rhapsodos12@testcluster.shgyv.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDb = process.env.MONGO_URI;
 } else {
-    urlDb = 'mongodb+srv://santacruz:rhapsodos12@testcluster.shgyv.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDb;
